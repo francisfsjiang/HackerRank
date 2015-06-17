7b499aa7b151382d6e193b7440c7e5b1f2369b8e
Neveralso <neveralso@gmail.com>
2015-06-17 11:52:00 +0800

    commit

---
+++ functional_programming/recursion/computing_the_gcd/solution.erl
@@ -0,0 +1,15 @@
+-module(solution).
+-export([main/0]).
+
+gcd(A, 0) ->
+    A;
+gcd(A, B) when A < B->
+    gcd(B, A);
+gcd(A, B) ->
+    gcd(B, A rem B).
+
+
+main() ->
+	{ok, [A, B]} = io:fread("", "~d~d"),
+    G = gcd(A, B),
+    io:format("~w~n", [G]).
